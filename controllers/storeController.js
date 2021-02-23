1858d5dfdac1de70ad72d409ede4839a4b2fa10c
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-02-22 19:23:52 -0800

    add tags page with filter of stores by tag

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -79,3 +79,14 @@ exports.getStoreBySlug = async (req, res) => {
   if (!store) return next();
   res.render('store', { store, title: store.name });
 }
+
+exports.getStoresByTag = async (req, res) => {
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true };
+
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+
+  res.render('tags', { tags, title: 'Tags', tag, stores });
+}
