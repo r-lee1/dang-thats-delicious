292968a29d04148425ac897490499a7932d5362a
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-02-26 23:19:28 -0800

    add store search with typeahead

---
+++ public/javascripts/modules/typeAhead.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+
+function searchResultsHTML(stores) {
+  return stores.map(store => {
+    return `
+      <a href="/store/${store.slug}" class="search__result">
+        <strong>${store.name}</strong>
+      </a>
+    `;
+  }).join("");
+}
+
+function typeAhead(search) {
+  if (!search) return;
+
+  const searchInput = search.querySelector('input[name="search"]');
+  const searchResults = search.querySelector('.search__results');
+  searchInput.on('input', function() {
+    // if there is no value, quit it!
+    if(!this.value) {
+      searchResults.style.display = 'none';
+      return;
+    }
+
+    // show the search results!
+    searchResults.style.display = 'block';
+    searchResults.innerHTML = '';
+
+    axios
+      .get(`/api/search?q=${this.value}`)
+      .then(res => {
+        if(res.data.length) {
+          console.log('There is something to show!');
+          searchResults.innerHTML = searchResultsHTML(res.data);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+}
+
+export default typeAhead;
